e3723c8fa2b468f600d2d3cd1b4c238f10c9d697
hamzasand <mhamzasaeed762@gmail.com>
2026-07-05 20:33:10 +0500

    add schema add authentication

---
+++ schemas.py
@@ -0,0 +1,15 @@
+from pydantic import BaseModel
+
+#input schema
+class BlogCreate(BaseModel):
+    title: str
+    content: str
+
+#Output Schema
+class BlogResponse(BaseModel):
+    id:int
+    title:str
+    content:str
+
+class Config:
+    from_attributes =True
