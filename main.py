ad486cd27ee13d072211ea0bf7adbf2de9842491
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-07-09 20:54:49 +0500

    Add import for SessionLocal from database

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from fastapi import FastAPI,Depends,HTTPException,Query
 
 from sqlalchemy.orm import Session
+
 from database import engine,SessionLocal
 import models,schemas
 from auth import create_token,verify_token
