07753a4ea73895789f9961d71057a2103c069427
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-07-11 13:19:42 +0500

    Update auth.py

--- auth.py
+++ auth.py
@@ -3,7 +3,6 @@ from dotenv import load_dotenv
 from jose import jwt,JWTError
 from datetime import datetime,timedelta,timezone
 from fastapi import HTTPException,Depends
-
 from fastapi.security import OAuth2PasswordBearer
 
 load_dotenv()
