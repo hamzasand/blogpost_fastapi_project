9b00bc3b466ae6a930803b18603eb93e881cdbfe
M.Hamza <152792061+hamzasand@users.noreply.github.com>
2026-07-13 21:24:27 +0500

    Update schemas.py

--- schemas.py
+++ schemas.py
@@ -1,13 +1,11 @@
 from pydantic import BaseModel
 
 #input schema
-
 class BlogCreate(BaseModel):
     title: str
     content: str
 
 #Output Schema
-
 class BlogResponse(BaseModel):
     id:int
     title:str
